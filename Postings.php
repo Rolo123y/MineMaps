1789c21e64ae1a514ad751fd978896677c242e6b
Ruhollah Nasim <ruhollah123@hotmail.com>
2020-03-31 13:51:37 -0400

    update Postings

--- Postings.php
+++ Postings.php
@@ -25,7 +25,7 @@ session_start();
             }
         </script>
 
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDt-GNBXCZi9mtSY8BdHtwluaNg31HdlKg&callback=initMap" async defer></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap" async defer></script>
 
     </div>
 
